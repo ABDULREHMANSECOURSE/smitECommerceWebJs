a6a2544da123668e937748adf687fe7e9790ee2e
ABDUL REHMAN <abdulrehmanaziz000@gmail.com>
2025-11-15 18:22:29 +0500

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+function displayNoneOrOther(element, currentDisplay) {
+    const el = document.querySelector(element)
+    el.style.display = el.style.display === currentDisplay ? "none" : currentDisplay
+}
