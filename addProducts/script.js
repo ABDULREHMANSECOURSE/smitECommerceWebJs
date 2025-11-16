84d774230aba16125c11b4d789ac4f2afc799851
ABDUL REHMAN <abdulrehmanaziz000@gmail.com>
2025-11-16 14:08:36 +0500

    .

---
+++ addProducts/script.js
@@ -0,0 +1,29 @@
+const viewBox = document.querySelector(".productOverView");
+const pImg = document.getElementById("pImg");
+const pName = document.getElementById("pName");
+const pPrice = document.getElementById("pPrice");
+const pCategory = document.getElementById("pCategory");
+
+let imgURL = "";
+
+pImg.addEventListener("change", () => {
+    const file = pImg.files[0];
+    imgURL = URL.createObjectURL(file);
+    updatePreview();
+});
+
+pName.addEventListener("input", updatePreview);
+
+pPrice.addEventListener("input", updatePreview);
+
+pCategory.addEventListener("change", updatePreview);
+
+
+function updatePreview() {
+    viewBox.innerHTML = `
+        ${imgURL ? `<img src="${imgURL}">` : ""}
+        <h3>${pName.value || ""}</h3>
+        <p>${pPrice.value ? "Price: $" + pPrice.value : ""}</p>
+        <p>${pCategory.value && pCategory.selectedIndex !== 0 ? "Category: " + pCategory.value : ""}</p>
+    `;
+}
--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function displayNoneOrOther(element, currentDisplay) {
     const el = document.querySelector(element)
     el.style.display = el.style.display === currentDisplay ? "none" : currentDisplay
 }
+localStorage.setItem('a',"aaa")
