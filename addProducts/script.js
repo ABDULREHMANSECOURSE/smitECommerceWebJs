a97d03fc73cbacdc7506d95f7c4a75ce1a724739
ABDUL REHMAN <abdulrehmanaziz000@gmail.com>
2025-11-16 14:09:34 +0500

    .

--- addProducts/script.js
+++ addProducts/script.js
@@ -3,22 +3,15 @@ const pImg = document.getElementById("pImg");
 const pName = document.getElementById("pName");
 const pPrice = document.getElementById("pPrice");
 const pCategory = document.getElementById("pCategory");
-
 let imgURL = "";
-
 pImg.addEventListener("change", () => {
     const file = pImg.files[0];
     imgURL = URL.createObjectURL(file);
     updatePreview();
 });
-
 pName.addEventListener("input", updatePreview);
-
 pPrice.addEventListener("input", updatePreview);
-
 pCategory.addEventListener("change", updatePreview);
-
-
 function updatePreview() {
     viewBox.innerHTML = `
         ${imgURL ? `<img src="${imgURL}">` : ""}
